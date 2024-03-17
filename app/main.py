c68a751d1e1e977a58fcf693f246ee96540e79cd
Anthony Magpantay <anthony040190@yahoo.com>
2024-03-17 00:22:17 -0600

    transfer the code from hugging face

--- app/main.py
+++ app/main.py
@@ -3,18 +3,39 @@ from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from app.middleware.processPrompt import promptResponse
 
-app = FastAPI()
+from transformers import ViTImageProcessor, ViTForImageClassification
+from PIL import Image
+import requests
 
-# Setup templates directory
-templates = Jinja2Templates(directory="app/templates")
+# app = FastAPI()
 
-@app.get("/", response_class=HTMLResponse)
-async def read_form(request: Request):
-    return templates.TemplateResponse("form.html", {"request": request})
+# # Setup templates directory
+# templates = Jinja2Templates(directory="app/templates")
 
-@app.post("/", response_class=HTMLResponse)
-async def process_form(request: Request, prompt: str = Form(...)):
-    # Here you can process the prompt and get the response
+# @app.get("/", response_class=HTMLResponse)
+# async def read_form(request: Request):
+#     return templates.TemplateResponse("form.html", {"request": request})
 
-    newResponse = promptResponse(prompt)
-    return templates.TemplateResponse("form.html", {"request": request, "response": newResponse})
+# @app.post("/", response_class=HTMLResponse)
+# async def process_form(request: Request, prompt: str = Form(...)):
+#     # Here you can process the prompt and get the response
+
+#     newResponse = promptResponse(prompt)
+#     return templates.TemplateResponse("form.html", {"request": request, "response": newResponse})
+
+
+
+
+
+url = 'http://images.cocodataset.org/val2017/000000039769.jpg'
+image = Image.open(requests.get(url, stream=True).raw)
+
+processor = ViTImageProcessor.from_pretrained('google/vit-base-patch16-224')
+model = ViTForImageClassification.from_pretrained('google/vit-base-patch16-224')
+
+inputs = processor(images=image, return_tensors="pt")
+outputs = model(**inputs)
+logits = outputs.logits
+# model predicts one of the 1000 ImageNet classes
+predicted_class_idx = logits.argmax(-1).item()
+print("Predicted class:", model.config.id2label[predicted_class_idx])
